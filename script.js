32c34c3f37a95ff0bde6961944024bab805af979
Ocean <trungduongnguyen2004@gmail.com>
2023-02-20 12:16:56 -0800

    Completing the first draft of the link

---
+++ script.js
@@ -0,0 +1,20 @@
+const enhance = id =>{/*take in an element id*/ 
+    const element = document.getElementById(id), /*use the taken id to select the element*/ 
+        text = element.innerText.split(""); /*take the element's and split into arrays of characters*/
+    element.innerText = ""; /*Clear out the existing text*/     
+    
+    text.forEach(letter =>{ /* Going thourgh the array*/
+
+        const span = document.createElement("span");  
+        span.className = "letter";
+        /* Create a span with class name "letter" for each character*/
+
+
+
+        span.innerText = letter;
+        /* Put the letter into the created span*/
+
+        element.appendChild(span); /* Put everything into the original element*/
+    })
+}
+enhance("message")
