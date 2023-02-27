9c9226042d0dc6dbebd7ec771166f66a8c3a4638
Ocean <trungduongnguyen2004@gmail.com>
2023-02-27 11:24:12 -0800

    format fix

--- script.js
+++ script.js
@@ -8,26 +8,16 @@ const enhance = id => {/*take in an element id*/
             const span = document.createElement("span");
             span.className = "hidden";
             /* Create a span with class name "letter" for each character*/
-
-
-
             span.innerText = letter;
             /* Put the letter into the created span*/
-
             element.appendChild(span); /* Put everything into the original element*/
-
-
         } else {
 
             const span = document.createElement("span");
             span.className = "letter";
             /* Create a span with class name "letter" for each character*/
-
-
-
             span.innerText = letter;
             /* Put the letter into the created span*/
-
             element.appendChild(span); /* Put everything into the original element*/
         }
     })
